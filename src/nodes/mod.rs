92659848534394fd9c88ec3736e67c54ea0c1c16
Maxime D. <xam4lor@gmail.com>
2026-08-26 14:52:36 +0200

    Removed test node

--- src/nodes/mod.rs
+++ src/nodes/mod.rs
@@ -1,9 +1,7 @@
 pub mod node_erosion;
 pub mod node_generator_flat;
 pub mod node_generator_perlin;
-pub mod node_test;
 
 pub use node_erosion::NodeErosion;
 pub use node_generator_flat::NodeGeneratorFlat;
 pub use node_generator_perlin::NodeGeneratorPerlin;
-pub use node_test::NodeTest;
